08a53b38d6d3dd4c41db075fb0ff31e44b3725f3
xuxinsheng <xuxinsheng@guazi.com>
2019-06-26 16:54:20 +0800

    jsx原理及createElement应用

--- src/App.js
+++ src/App.js
@@ -1,26 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { createElement } from 'react';
 import './App.css';
 
+// 该实现最终会编译为ButtonElement 形式 可以根据webpack打包结果对比
+const Button = ({ color = 'red', text = 'Button' }) =>
+        <button style={{ color }}>
+            <span>{text}</span> <em>{color}</em>
+        </button>
+
+// webpack 打包后结果
+// const Button = ({
+//     color = 'red',
+//     text = 'Button'
+//   }) => react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("button", {
+//     style: {
+//       color
+//     },
+//     __source: {
+//       fileName: _jsxFileName,
+//       lineNumber: 6
+//     },
+//     __self: undefined
+//   }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("span", {
+//     __source: {
+//       fileName: _jsxFileName,
+//       lineNumber: 7
+//     },
+//     __self: undefined
+//   }, text), " ", react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("em", {
+//     __source: {
+//       fileName: _jsxFileName,
+//       lineNumber: 7
+//     },
+//     __self: undefined
+//   }, color));
+
+
+// JSX 将 HTML 语法直接加入到 JavaScript 代码中，再通过翻译器转换到纯 JavaScript 后由浏览器执行
+const ButtonElement = ({ color = 'red', text = 'Button' }) => {
+    return React.createElement('button', { style: { color } }, [
+        React.createElement('span', { key: 'span' }, text),
+        ' ',
+        createElement('em', { key: 'em' }, color)
+    ]);
+};
+
+// webpack 打包后结果
+// const ButtonElement = ({
+//   color = 'red',
+//   text = 'Button'
+// }) => {
+//   return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement('button', {
+//     style: {
+//       color
+//     }
+//   }, [react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement('span', {
+//     key: 'span'
+//   }, text), ' ', Object(react__WEBPACK_IMPORTED_MODULE_0__["createElement"])('em', {
+//     key: 'em'
+//   }, color)]);
+// };
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Button />
+            <ButtonElement />
+        </div>
+    );
 }
 
 export default App;
