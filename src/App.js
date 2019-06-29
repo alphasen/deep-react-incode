a5f1592a27d7e227dd9e4c79405e26fbd6b0e2d5
xuxinsheng <xuxinsheng@guazi.com>
2019-06-29 22:14:17 +0800

    css-modules实验

--- src/App.js
+++ src/App.js
@@ -1,28 +1,39 @@
-import React,{Component} from 'react';
-import PropTypes from 'prop-types'
-import classnames from 'classnames'
-import {pure} from 'recompose'
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import classnames from 'classnames';
+import { pure } from 'recompose';
+import './App.css';
+import styles from './app.module.scss';
 
-class App extends Component{
-    static propTypes={
+console.log('styles :', styles);
+class App extends Component {
+    static propTypes = {};
 
+    static defaultProps = {};
+
+    constructor(props) {
+        super(props);
+        this.state = {};
     }
 
-    static defaultProps={
-
-    }
-
-    constructor(props){
-        super(props)
-        this.state={
-
-        }
-    }
-
-    render(){
-        return <div>
-
-        </div>
+    render() {
+        let classes = classnames({
+            [styles.blue]: true,
+            bg: true,
+            [styles.font]: true
+        });
+        return (
+            <div>
+                <div className={classes}>
+                    blue<span>span</span>
+                    <div className={styles.yellow}>yellow in scss's sub classes</div>
+                </div>
+                <div className={styles.yellow}>yello22222</div>
+                <div className={styles.compose}>composes</div>
+                <div className={styles.green}>green from App.css</div>
+                <div className={styles['variable-green']}>variable-green</div>
+            </div>
+        );
     }
 }
 
