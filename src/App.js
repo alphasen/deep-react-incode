edbcbda2b8936970f8baf29dbbced58bcf814d8e
xuxinsheng <xuxinsheng@guazi.com>
2019-06-30 17:27:20 +0800

    使用事件处理组件间通信问题

--- src/App.js
+++ src/App.js
@@ -1,28 +1,77 @@
-import React,{Component} from 'react';
-import PropTypes from 'prop-types'
-import classnames from 'classnames'
-import {pure} from 'recompose'
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import classnames from 'classnames';
+import { pure } from 'recompose';
+import emitter from './event';
 
-class App extends Component{
-    static propTypes={
+function Button({ text = 'button' }) {
+    return (
+        <button
+            onClick={() => {
+                emitter.emit('childChange', { a: 'bbb' });
+            }}
+        >
+            {text}
+        </button>
+    );
+}
 
+class Child extends Component {
+    componentDidMount(){
+        emitter.on('parentClick',(data)=>console.log(data))
+    }
+    componentWillUnmount(){
+        emitter.removeListener('parentClick')
+    }
+    render() {
+        return (
+            <div>
+                <Button text="btn" />
+                <div
+                    onClick={() => {
+                        emitter.emit('childChange', { a: 'aaa' });
+                    }}
+                >
+                    Child
+                </div>
+            </div>
+        );
+    }
+}
+
+class App extends Component {
+    static propTypes = {};
+
+    static defaultProps = {};
+
+    constructor(props) {
+        super(props);
+        this.state = {};
     }
 
-    static defaultProps={
-
+    componentDidMount() {
+        this.emitter = emitter.on('childChange', data => {
+            console.log(data);
+        });
     }
 
-    constructor(props){
-        super(props)
-        this.state={
-
-        }
+    componentWillUnmount(){
+        emitter.removeListener('childChange')
     }
 
-    render(){
-        return <div>
-
-        </div>
+    render() {
+        return (
+            <div>
+                <Child />
+                <button
+                    onClick={() => {
+                        emitter.emit('parentClick', '999');
+                    }}
+                >
+                    parent
+                </button>
+            </div>
+        );
     }
 }
 
